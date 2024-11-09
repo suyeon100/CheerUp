9b62832934c9be9b93d7ea43a5063831043f61e3
suyeon100 <xxyx7115@gmail.com>
2024-11-09 15:59:51 +0900

    commit2

--- CheerUP/ReadTableViewCell.swift
+++ CheerUP/ReadTableViewCell.swift
@@ -2,16 +2,13 @@
 //  ReadTableViewCell.swift
 //  CheerUP
 //
-//  Created by 박수연 on 11/6/24.
+//  Created by 박수연 on 11/14/24.
 //
 
 import UIKit
 
 class ReadTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var diary: UILabel!
-    var addMoreClickedHandler: (()->())?
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -19,8 +16,8 @@ class ReadTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        
 
+        // Configure the view for the selected state
     }
     
 }
